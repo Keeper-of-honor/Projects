0e3dde67c0fdbbcdf9b502586215608a915a3ffd
Keeper-of-honor <keepka18@gmail.com>
2020-10-29 23:55:23 +0300

    Сделал почти все меню. Нужно сделать создание карты

--- Zavod/Наработки/main/helpFunc.cpp
+++ Zavod/Наработки/main/helpFunc.cpp
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <unistd.h>
 
+//------------------------------------------------
+//Глобальные константы
+extern const int WIDTH_WIN;	// Ширина окна меню
+extern const int HEIGHT_WIN;	// Длина окна меню
+
+extern const int LEN;	//Длина
+
 //Анимация
 //	Передаём площадь экрана
 void animation(int s_win){
@@ -27,7 +34,7 @@ void animation(int s_win){
 
 //Блок = строка. Например, если pos_cursor = 0, то блок = "Run"
 //Печать блока
-void printBlock(int pos_cursor, const int WIDTH_WIN, const int NUM_BLOCK, char text[]){
+void printBlock(int pos_cursor, const int NUM_BLOCK, char text[]){
 	size_t len_str = strlen(text);	//Длина блока
 
 	size_t y = LINES/2 - (NUM_BLOCK / 2) + pos_cursor;	//Номер строки блока
@@ -48,8 +55,14 @@ void printBlock(int pos_cursor, const int WIDTH_WIN, const int NUM_BLOCK, char t
 		mvaddch(y, i, ' ');
 }
 
+//Печать всех блоков
+void printAllBlocks(const int NUM_BLOCK, char text[]){
+	for (int i = 0; i != NUM_BLOCK; ++i)
+		printBlock(i, NUM_BLOCK, &text[i * LEN]);	//Печать блока
+}
+
 //Выделение блока (строки)
-void selectBlock(int pos_cursor, const int WIDTH_WIN, const int NUM_BLOCK, char text[]){
+void selectBlock(int pos_cursor, const int NUM_BLOCK, char text[]){
 	size_t len_str = strlen(text);	//Длина блока
 
 	size_t y = LINES/2 - (NUM_BLOCK / 2) + pos_cursor;	//Номер строки блока
@@ -75,15 +88,36 @@ void selectBlock(int pos_cursor, const int WIDTH_WIN, const int NUM_BLOCK, char 
 }
 
 //Навигация
-void navigation(int ch, int &pos_cursor, const int NUM_BLOCK){
-	if (ch == KEY_DOWN){
+void navigation(int &ch, int &pos_cursor, const int NUM_BLOCK, char text[]){
+	ch = getch();
+
+	printBlock(pos_cursor, NUM_BLOCK, &text[pos_cursor * LEN]);	//Печать блока
+	
+	if (ch == KEY_DOWN || ch == 's'){
 		++pos_cursor;
 		if (pos_cursor == NUM_BLOCK)
 			pos_cursor = 0;
 	}
-	else if (ch == KEY_UP){
+	else if (ch == KEY_UP || ch == 'w'){
 		--pos_cursor;
 		if (pos_cursor == -1)
 			pos_cursor = NUM_BLOCK - 1;
 	}
+	if ( ch > '0' && ch <= NUM_BLOCK + '0' )
+		pos_cursor = ch - '0' - 1;
+	
+	selectBlock(pos_cursor, NUM_BLOCK, &text[pos_cursor * LEN]);	//Выделение блока
 }
+/*
+
+//Обновление переменных и вывод всех блоков и выделение первого
+void dataUpdate(int &depth_menu, int &pos_cursor, const int NUM_BLOCK, char text[]){
+	//Обновление переменных
+	--depth_menu;
+	pos_cursor = 0;
+
+	printAllBlocks(NUM_BLOCK, &text[0]);	//Выписывает весь список игр
+	selectBlock(pos_cursor, NUM_BLOCK, &text[0]);	//Выделение блока
+}
+
+*/
--- Zavod/Наработки/main/mainFunc.cpp
+++ Zavod/Наработки/main/mainFunc.cpp
@@ -4,22 +4,26 @@
 
 #include "helpFunc.h"
 
+//------------------------------------------------
+//Глобальные константы
+extern const int WIDTH_WIN;	// Ширина окна меню
+extern const int HEIGHT_WIN;	// Длина окна меню
+
+extern const int LEN;	//Длина
+
 //Первоначальная анимация и создание границ
-void borderMenu(const int WIDTH_WIN, const int HEIGHT_WIN, const int LEN){
-	
+void borderMenu(){
+
 	int offsetx = (COLS - WIDTH_WIN) / 2;
 	int offsety = (LINES - HEIGHT_WIN) / 2;
 
 	WINDOW *win = newwin(HEIGHT_WIN, WIDTH_WIN, offsety, offsetx);
 
 	//Ввод никнейма
-	char* username;
-	mvprintw(LINES / 2 - 2, COLS / 2 - LEN + 2, "Enter name :  ");
-	scanw("%s", &username);
-
-	//Анимация перед чертением
-	int s_win = COLS  * LINES;	// Площадь консоли
-	animation(s_win);
+	//char username[LEN];
+	//mvprintw(LINES / 2 - 2, COLS / 2 - LEN + 10, "Enter name :  ");
+	//scanw("%s", &username);
+	//mvprintw(1, 0, "%s", username);	
 
 	//Чертим границы
 	box(win, 0, 0);
@@ -29,7 +33,7 @@ void borderMenu(const int WIDTH_WIN, const int HEIGHT_WIN, const int LEN){
 }
 
 //Вывод имени игры
-void nameGame(const int HEIGHT_WIN, char name_app[]){
+void nameGame(char name_app[]){
 	size_t len_str = strlen(name_app);
 	size_t y = (LINES - HEIGHT_WIN) / 2 + 3;
 	size_t x = (COLS - len_str) / 2;
